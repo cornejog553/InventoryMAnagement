1e5f9daf3defe482a2a047ef0e44343ce9e0ddfc
Giovanni Cornejo <cornejog553@gmail.com>
2024-10-06 00:39:14 -0400

    finished content of index page including conferences, players, and all jerseys

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const playerRouter = require('./routes/playerRoutes')
 const app = express();
 const path = require("node:path");
+const db = require("./db/queries");
 
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-    res.render("index",{message:"Go Lakers!"});
-});
+app.get("/", getTeams);
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -18,8 +17,20 @@ app.use((req, res, next) => {
     next();
   })
 
-// base mount path is `/users` and will always execute on that specific mount path, and yes including `/users/a/b/c`
+// base mount path is `/players` and will always execute on that specific mount path, and yes including `/users/a/b/c`
 app.use('/players', playerRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
+
+async function getTeams(req, res) {
+    const easternteams = await db.getEasternTeams();
+    const westernteams = await db.getWesternTeams();
+    const playerImages = await db.getPlayersImage();
+    const jerseyImages = await db.getJerseysImage()
+    res.render("index", {easternteams: easternteams,
+      westernteams: westernteams,
+      playerimages: playerImages,
+      jerseyimages: jerseyImages
+    });
+}
--- db/queries.js
+++ db/queries.js
@@ -5,10 +5,39 @@ async function getAllPlayers() {
   return rows;
 }
 
+async function getAllTeams() {
+  const { rows } = await pool.query("SELECT image_link FROM teams");
+  return rows;
+}
+
+async function getEasternTeams() {
+  const { rows } = await pool.query("SELECT image_link FROM teams WHERE conference = 'Eastern'");
+  return rows;
+}
+
+async function getWesternTeams() {
+  const { rows } = await pool.query("SELECT image_link FROM teams WHERE conference = 'Western'");
+  return rows;
+}
+
+async function getPlayersImage() {
+  const { rows } = await pool.query("SELECT image_link FROM player");
+  return rows;
+}
+
+async function getJerseysImage() {
+  const { rows } = await pool.query("SELECT image_link FROM jerseys");
+  return rows;
+}
+
 // async function insertUsername(username) {
 //   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
 // }
 
 module.exports = {
-  getAllPlayers
+  getAllPlayers,
+  getEasternTeams,
+  getWesternTeams,
+  getPlayersImage,
+  getJerseysImage
 };
